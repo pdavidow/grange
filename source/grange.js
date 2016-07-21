5b68e525b6ac7570d4124746b38486365673a7b4
pdavidow <pdavidow@fastmail.fm>
2016-07-21 15:53:41 -0500

    first test: work in progress

---
+++ source/grange.js
@@ -0,0 +1,3 @@
+const grange = ()=>[3,4,5,6];
+
+module.exports = grange;
--- source/test/index.js
+++ source/test/index.js
@@ -1,1 +1,18 @@
-throw new Error('No tests!');
+// babel-node index.js
+
+const test = require('tape');
+const grange = require('../grange');
+
+test('Create a simple range from start to end, inclusive', (assert) => {
+    const msg = 'simple range';
+
+    const [...actual1] = grange(3, 6);
+    const expected1 = [3, 4, 5, 6];
+    assert.same(actual1, expected1, msg);
+
+    const [...actual2] = grange(4, 7);
+    const expected2 = [4, 5, 6, 7];
+    assert.same(actual2, expected2, msg);
+
+    assert.end();
+});
